e1d65b67f026de0113b756c29979c8d87571dcc6
Jamie Newbigging <jamienewbigging@gmail.com>
2020-06-04 21:25:07 +0100

    Updates

---
+++ precache-manifest.fde2b91786e50075529ad4e52397e705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420711c998c76d3a882a30aebf422f9b",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "5127e7ba2c04cabf6fcb",
+    "url": "/calculator/static/css/main.bd43c97a.chunk.css"
+  },
+  {
+    "revision": "29b0f5a4012ddceb0776",
+    "url": "/calculator/static/js/2.7e947bb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.7e947bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5127e7ba2c04cabf6fcb",
+    "url": "/calculator/static/js/main.f50409f5.chunk.js"
+  },
+  {
+    "revision": "53122c127a7fe401835e",
+    "url": "/calculator/static/js/runtime-main.6de0c573.js"
+  }
+]);
